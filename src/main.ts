b1b34f39f5b55afb697539b2b455ca1eb6e37e1f
Oleksa Panchuk <opanchuk@cloverdynamics.com>
2025-11-19 13:06:44 +0200

    Initialize NestJS project with core setup:
    - Add `.gitignore` for Node.js environment.
    - Set up Prettier and ESLint configurations.
    - Implement basic NestJS structure: `AppController`, `AppService`, and `main.ts`.
    - Add `docker-compose.yml` for containerized services.
    - Configure package scripts, Jest testing, and dependencies in `package.json`.

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { Logger } from '@nestjs/common';
+
+async function bootstrap() {
+	const app = await NestFactory.create(AppModule);
+	await app.listen(process.env.PORT ?? 8080);
+  Logger.log(`Server is running on port ${process.env.PORT ?? 8080}`);
+}
+bootstrap();
